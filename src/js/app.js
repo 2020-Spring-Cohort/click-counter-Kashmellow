7647d20fb03012361bcf4dab6d4bc0e2588700ed
Kashmellow <ejodlowsky@pm.me>
2020-03-05 13:09:30 -0500

    added cookie counter button, clicking counter function, reset button

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,21 @@
 const getCookie = function(greeting) {
     return greeting;
 }
+
+function cookieClick() {
+
+    console.log('Click the Cookie!');
+}
+
+    var count = 0;
+const countButton = document.getElementById("countButton");
+const displayCount = document.getElementById("displayCount");
+countButton.onclick = function(){
+  count++;
+  displayCount.innerHTML = count;
+}
+resetButton.onclick = function(){
+  count = 0;
+  displayCount.innerHTML = count;
+}
+
