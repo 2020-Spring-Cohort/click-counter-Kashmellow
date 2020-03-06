ac0fb4b4ba2f7a0863a47b689cf7eff9a191e62a
Kashmellow <ejodlowsky@pm.me>
2020-03-06 11:24:25 -0500

    refactoring according to recent guidelines, WIP

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,37 @@
-const getCookie = function(greeting) {
-    return greeting;
+// const getCookie = function(greeting) {
+//     return greeting;
+// }
+
+// var count = 0;
+// const countButton = document.getElementById("countButton");
+// const displayCount = document.getElementById("displayCount");
+// countButton.onclick = function(){
+//   count++;
+//   displayCount.innerHTML = count;
+// }
+// resetButton.onclick = function(){
+//   count = 0;
+//   displayCount.innerHTML = count;
+// }
+
+// if (displayCount === 10) {
+//     alert("You have earned a Clicking Companion!");
+// }
+
+const updateClicker = (dashboardElement, cookieObject) => {
+    dashboardElement.innerText = cookieObject.getCookieCount();
 }
 
-var count = 0;
-const countButton = document.getElementById("countButton");
-const displayCount = document.getElementById("displayCount");
-countButton.onclick = function(){
-  count++;
-  displayCount.innerHTML = count;
-}
-resetButton.onclick = function(){
-  count = 0;
-  displayCount.innerHTML = count;
+const makeAcceleratorButton = (buttonClickElement, dashboardElement, cookieObject) => {
+    buttonClickElement.addEventListener('click', ()=>{
+        cookieObject.cookieCounter();
+        updateClicker(dashboardElement, cookieObject);
+    })
 }
 
-if (displayCount === 10) {
-    alert("You have earned a Clicking Companion!");
-}
+const buttonClickElement = document.querySelector('.floorboard__accelerator');
+const dashboardElement = document.querySelector('.dashboard__cookieCounter');
+const myCookie = new Cookie();
+
+makeAcceleratorButton(buttonClickElement, dashboardElement, myCookie);
+updateClicker(dashboardElement, myCookie);
